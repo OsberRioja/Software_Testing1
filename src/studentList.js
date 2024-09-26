17bd62a41fb0cc4b29aaf117ccb954ddf63de5dc
Osber <osberriojalopez@gmail.com>
2024-09-26 02:05:59 -0400

    refactoring comented

--- src/studentList.js
+++ src/studentList.js
@@ -108,5 +108,26 @@ function getStudentsInCourse(courseToCheck)
     }
     return students
 }
+// function getStudentsInCourse(courseToCheck) {
+//     let students = {};
+    
+//     function incrementCourseCount(course) {
+//         students[course] = (students[course] || 0) + 1;
+//     }
+
+//     for (const student of Object.values(studentDict)) {
+//         let courses = student.getCoursesStudent();
+//         if (courses.has(courseToCheck)) {
+//             incrementCourseCount(courseToCheck);
+//             courses.forEach(course => {
+//                 if (course !== courseToCheck) {
+//                     incrementCourseCount(course);
+//                 }
+//             });
+//         }
+//     }
+
+//     return students;
+// }
 
 export { getCoursesFromAllStudents, getCoursesFromAllStudentsWithinACourse, getStudentName, getStudents, getStudentsFromJson, getStudentsInCourse, seeIfStudentExist, setStudentName, studentLogIn };
